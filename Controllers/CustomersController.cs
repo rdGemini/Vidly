2645d2d23cf05bed10d308f8fb6979e64024e71a
Rohit Kumar <rohitgemini1992@gmail.com>
2024-08-27 00:23:12 +0530

    Updating New Movie - section 4 exercise

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -23,12 +23,28 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
 
-            return View(viewModel);
+            return View("CustomerForm",viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customersInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customersInDb.Name = customer.Name;
+                customersInDb.Birthdate = customer.Birthdate;
+                customersInDb.MembershipTypeId = customer.MembershipTypeId;
+                customersInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
         }
 
         public ViewResult Index()
@@ -45,6 +61,19 @@ namespace Vidly.Controllers
                 return HttpNotFound();
 
             return View(customer);
+        }
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
 
     }
--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using Vidly.Models;
 using Vidly.ViewModels;
 using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+using System;
+using Vidly.Migrations;
 
 namespace Vidly.Controllers
 {
@@ -27,23 +30,29 @@ namespace Vidly.Controllers
             return View(movies);
         }
 
-        
-        // GET: Movies/Random
-        public ActionResult Random()
+        public ViewResult New()
         {
-            var movies = new List<Movie>
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
             {
-                new Movie { Name = "Shrek" },
-                new Movie { Name = "wall-e" }
-
+                Genres = genres
             };
-            var viewModel = new RandomMovieViewModel
-            {
-                Movies = movies
-            };
-            return View(viewModel);
+            return View("MovieForm", viewModel);
         }
 
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+            return View("MovieForm", viewModel);
+        }
         public ActionResult Details(int id)
         {
             var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
@@ -52,6 +61,44 @@ namespace Vidly.Controllers
                 return HttpNotFound();
 
             return View(movie);
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
     }
 }
